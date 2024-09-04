2a639c736e83e209e0ff6fb259a4741bfe38b80f
Liara Cristina <lidskey@hotmail.com>
2024-09-04 11:32:32 -0300

    feat: adicionado ROTAS

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import './App.css'
 import NavBar from './components/navbar/Navbar'
 import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListaCategorias from './components/categorias/listacategoria/ListarCategorias'
+import FormCategoria from './components/categorias/formcategoria/FormCategoria'
+import DeletarCategoria from './components/categorias/deletarcategorias/DeletarCategoria'
 
 function App() {
   
 
   return (
     <>
-      <NavBar />
-      <Home />
-      <Footer/>
+      <BrowserRouter>
+        <NavBar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+            <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+            <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+          </Routes>
+            </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
